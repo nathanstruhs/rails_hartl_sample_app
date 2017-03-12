5c8e8aaf66b822f6d76434640894dd4e3c8256a1
Nathan Struhs <nathanstruhs@gmail.com>
2017-03-12 13:24:06 -0700

    Completed section 7 exercises

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -12,6 +12,8 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                password_confirmation: "bar" }
     end
     assert_template 'users/new'
+    assert_select '#error_explanation'
+    assert_select '.field_with_errors'
   end
 
   test "valid signup information" do
@@ -23,6 +25,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                             password: "password" }
     end
     assert_template 'users/show'
+    assert_not flash.empty?
   end
 
 end
